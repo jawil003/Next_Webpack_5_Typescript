e619effcea26a470a4b5c97833569cda7ea57b1f
Jannik Will <jw19980309@gmail.com>
2021-06-30 07:20:11 +0200

    Initial Commit

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,31 @@
+const path = require('path');
+
 module.exports = {
+  core: {
+    builder: "webpack5",
+  },
   stories: ['../stories/*.stories.@(ts|tsx|js|jsx|mdx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+
+    config.module.rules.push({
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },);
+
+    // Return the altered config
+    return config;
+  },
 }
